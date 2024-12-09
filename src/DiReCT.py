7013a37309af5e1260fc6071fccb656a745bb2a3
Timo Blattner <timoblattner@Timos-MacBook-Air.local>
2024-12-09 22:06:04 +0100

    new interpolation method

---
+++ src/DiReCT.py
@@ -0,0 +1,174 @@
+import numpy as np
+import scipy.special as scipy_special
+import nibabel as nib
+import os
+import sys
+import argparse
+import ants
+from ants.internal import get_lib_fn, get_pointer_string 
+from ants.core import ants_image as iio
+
+
+# OVERWRITE
+def kelly_kapowski(s, g, w, its=45, r=0.025, m=1.5, gm_label=2, wm_label=3, **kwargs):
+    """
+    Compute cortical thickness using the DiReCT algorithm
+    """
+    if ants.is_image(s):
+        s = s.clone('unsigned int')
+
+    d = s.dimension
+    outimg = g.clone() * 0.0
+    kellargs = {'d': d,
+                's': "[{},{},{}]".format(get_pointer_string(s),gm_label,wm_label),
+                'g': g,
+                'w': w,
+                'c': "[{}]".format(its),
+                'r': r,
+                'm': m,
+                'o': outimg}
+    for k, v in kwargs.items():
+        kellargs[k] = v
+
+    processed_kellargs = process_arguments(kellargs)
+    libfn = get_lib_fn('KellyKapowski')
+    libfn(processed_kellargs)
+
+    return outimg
+
+# OVERWRITE
+def process_arguments(args):
+    """
+    Needs to be better validated.
+    """
+    p_args = []
+    if isinstance(args, dict):
+        for argname, argval in args.items():
+            if "-MULTINAME-" in argname:
+                # have this little hack because python doesnt support
+                # multiple dict entries w/ the same key like R lists
+                argname = argname[: argname.find("-MULTINAME-")]
+            if argval is not None:
+                if len(argname) > 1:
+                    p_args.append("--%s" % argname)
+                else:
+                    p_args.append("-%s" % argname)
+
+                if isinstance(argval, iio.ANTsImage):
+                    p_args.append(_ptrstr(argval.pointer))
+                elif isinstance(argval, list):
+                    p = "["
+                    for av in argval:
+                        if isinstance(av, iio.ANTsImage):
+                            av = _ptrstr(av.pointer)
+                        elif str(av) == "True":
+                            av = str(1)
+                        elif str(av) == "False":
+                            av = str(0)
+                        p += av + ","
+                    p += "]"
+
+                    p_args.append(p)
+                else:
+                    p_args.append(str(argval))
+
+    elif isinstance(args, list):
+        for arg in args:
+            if isinstance(arg, iio.ANTsImage):
+                pointer_string = _ptrstr(arg.pointer)
+                p_arg = pointer_string
+            elif arg is None:
+                pass
+            elif str(arg) == "True":
+                p_arg = str(1)
+            elif str(arg) == "False":
+                p_arg = str(0)
+            else:
+                p_arg = str(arg)
+            p_args.append(p_arg)
+    return p_args
+
+def _ptrstr(pointer):
+    """ get string representation of a py::capsule (aka pointer) """
+    libfn = get_lib_fn("ptrstr")
+    return libfn(pointer)
+
+def save_img(img, dst, name, ref_img):
+    fname = '{}/{}.nii.gz'.format(dst, name)
+    niftiImg = nib.Nifti1Image(img, ref_img.affine)
+    niftiImg.header['xyzt_units'] = 2  # mm
+    nib.save(niftiImg, fname)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Prepare¨ input and run DiReCT')
+    parser.add_argument('--prepare-only', type=bool, required=False, default=False, help='Prepare input only, skip DiReCT.')
+    parser.add_argument("source_dir")
+    parser.add_argument("destination_dir")
+    args = parser.parse_args()
+    
+    src = args.source_dir
+    dst = args.destination_dir
+    
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    
+    GM_labels = ['Left-Cerebral-Cortex', 'Right-Cerebral-Cortex', 'Left-Amygdala', 'Right-Amygdala', 'Left-Hippocampus', 'Right-Hippocampus']
+    WM_labels = ['Left-Cerebral-White-Matter', 'Right-Cerebral-White-Matter'] + (['WM-hypointensities'] if os.path.exists('{}/seg_WM-hypointensities.nii.gz'.format(src)) else [])
+    
+    # to get affine
+    ref_img = nib.load('{}/seg_{}.nii.gz'.format(src, WM_labels[0]))
+
+    gm_logit_all = np.stack(
+        [nib.load('{}/seg_{}.nii.gz'.format(src, label)).get_fdata(dtype=np.float32) for label in GM_labels])
+    
+    wm_logit_all = np.stack(
+        [nib.load('{}/seg_{}.nii.gz'.format(src, label)).get_fdata(dtype=np.float32) for label in WM_labels])
+    
+    gm_logit = np.max(gm_logit_all, axis=0)
+    wm_logit = np.max(wm_logit_all, axis=0)
+    
+    # handle background correctly (must remain zero!!)
+    gm_prob = np.where(gm_logit == 0, 0, scipy_special.expit(gm_logit))
+    wm_prob = np.where(wm_logit == 0, 0, scipy_special.expit(wm_logit))
+    
+    # fill holes (especially between GM/WM boundary): use argmax if combined probability > 0.7 but individual below 0.5
+    seg_img = (np.argmax([gm_prob, wm_prob], axis=0)+2).astype(np.uint8)
+    seg_img[gm_prob+wm_prob < 0.7] = 0
+    
+    seg_img[gm_prob > 0.5] = 2
+    seg_img[wm_prob > 0.5] = 3
+    
+    
+    # (before thresholding)
+    save_img(gm_prob, dst, 'gmprob', ref_img)
+    save_img(wm_prob, dst, 'wmprob', ref_img)
+    
+    # thresholding
+    # (need to make sure we actually have a gray/white matter interface
+    # which is defined as direct neighbouring GMprob > 0.5 with a neighbouring WMprob > 0.5)
+    T=0.5
+    
+    gm_prob[wm_prob > gm_prob] = 0
+    wm_prob[wm_prob > gm_prob] = 1
+    wm_prob[seg_img < 1] = 0
+    gm_prob[gm_prob > T] = 1
+    wm_prob[gm_prob > T] = 0
+    
+    save_img(seg_img, dst, 'seg', ref_img)
+    save_img(gm_prob, dst, 'gmprobT', ref_img)
+    save_img(wm_prob,dst,  'wmprobT', ref_img)
+    
+    if not args.prepare_only:
+        # run DiReCT (KellyKapowski), equivalent to
+        #     KellyKapowski -d 3 -s ${DST}/seg.nii.gz -g ${DST}/gmprobT.nii.gz -w ${DST}/wmprobT.nii.gz -o ${THICK_VOLUME} -c "[ 45,0.0,10 ]" -v
+        #thick = ants.kelly_kapowski(s=ants.from_numpy(seg_img), g=ants.from_numpy(gm_prob), w=ants.from_numpy(wm_prob), c='[ 45,0.0,10 ]', v='1')
+        thick = ants.from_numpy(wm_prob.copy())
+        kelly_kapowski(s=ants.from_numpy(seg_img), g=ants.from_numpy(gm_prob), w=ants.from_numpy(wm_prob), c='[ 10,0.0,10 ]', v='1', o=[thick, dst+"/T1w_"])
+
+        # Check thickness is not still all zeros
+        if thick.sum() == 0.0:
+            raise RuntimeError("KellyKapowski failed to compute thickness")
+
+        save_img(thick.numpy(), dst, 'T1w_thickmap', ref_img)
+    
---
+++ src/stats2table.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import argparse
+import sys
+import glob
+import os
+import re
+
+
+REGEX_SUBCORT = '^(?!lh-|rh-).*'
+REGEX_LH = '(SUBJECT.*)|(^lh.*)'
+REGEX_RH = '(SUBJECT.*)|(^rh.*)'
+
+
+def collect_stats(directory, filename):
+    df = None
+    for f in sorted(glob.glob('{}/*/{}'.format(directory, filename))):
+        df_subj = pd.read_csv(f, dtype=str)
+        df = df_subj if df is None else pd.concat([df, df_subj], ignore_index=True, sort=False)
+    
+    atlas = 'aparc'
+    if not df is None and 'G_and_S' in ''.join(df.columns):
+        atlas = 'aparca2009s'
+        df.columns = df.columns.str.replace('_and_', '&')
+        
+    return df, atlas
+
+
+def write_results(df, pattern, dst, suffix=''):
+    print('Writing {} rows to {}'.format(len(df), dst))
+    df_subset = df.filter(regex=pattern, axis=1)
+    # rename to match FS convention (lh- --> lh_ and add _suffix)
+    df_subset = df_subset.rename(columns=lambda x: re.sub('(lh|rh)-', r'\1_',x) + (suffix if not x.startswith('SUBJECT') else ''))
+    df_subset.to_csv(dst, sep='\t', na_rep='NaN', index=False)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Collect results from processed subjects and generate FreeSurfer alike stats2table files.')
+
+    parser.add_argument(
+        'subjects_dir',
+        type=str,
+        help='Directory containing processed subjects with DL+DiReCT results.'
+    )
+    
+    parser.add_argument(
+        'destination',
+        type=str,
+        help='Target directory (e.g. stats2table) for generated stats files.'
+    )
+
+    args = parser.parse_args()
+    if not os.path.exists(args.destination):
+        os.makedirs(args.destination, exist_ok=True)
+    
+    volumes, atlas = collect_stats(args.subjects_dir, 'result-vol.csv')
+    if volumes is not None:
+        write_results(volumes, REGEX_LH, '{}/lh.{}_stats_volume.txt'.format(args.destination, atlas), '_volume')
+        write_results(volumes, REGEX_RH, '{}/rh.{}_stats_volume.txt'.format(args.destination, atlas), '_volume')
+        
+        # add cortex vol as sum of parcellations
+        volumes['lhCortexVol'] = volumes.filter(regex=REGEX_LH, axis=1).drop('SUBJECT', axis=1).astype(float).sum(axis=1).astype(int)
+        volumes['rhCortexVol'] = volumes.filter(regex=REGEX_RH, axis=1).drop('SUBJECT', axis=1).astype(float).sum(axis=1).astype(int)
+        volumes['TotalCortexVol'] = volumes['lhCortexVol'] + volumes['rhCortexVol']
+        write_results(volumes, REGEX_SUBCORT, '{}/aseg_stats_volume.txt'.format(args.destination))
+        
+    
+    thick, atlas = collect_stats(args.subjects_dir, 'result-thick.csv')
+    if thick is not None:
+        write_results(thick, REGEX_LH, '{}/lh.{}_stats_thickness.txt'.format(args.destination, atlas), '_thickness')
+        write_results(thick, REGEX_RH, '{}/rh.{}_stats_thickness.txt'.format(args.destination, atlas), '_thickness')
+    
+    thickstd, atlas = collect_stats(args.subjects_dir, 'result-thickstd.csv')
+    if thickstd is not None:
+        write_results(thickstd, REGEX_LH, '{}/lh.{}_stats_thicknessstd.txt'.format(args.destination, atlas), '_thicknessstd')
+        write_results(thickstd, REGEX_RH, '{}/rh.{}_stats_thicknessstd.txt'.format(args.destination, atlas), '_thicknessstd')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
